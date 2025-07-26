da2d4777d7d40618f54d1ba8b68ba004d64ea8f3
IsxImattI <matej.gy@gmail.com>
2025-07-26 11:45:44 +0200

    Added .xlsx input support with ClosedXML and unit test

---
+++ EmailChecker/EmailInput.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using ClosedXML.Excel;
+
+namespace EmailChecker
+{
+    public static class EmailInput
+    {
+        public static List<string> ReadFromXlsx(string path)
+        {
+            var emails = new List<string>();
+            using var workbook = new XLWorkbook(path);
+            var worksheet = workbook.Worksheets.First();
+
+            foreach (var row in worksheet.RowsUsed())
+            {
+                var cell = row.Cell(1).GetString();
+                if (!string.IsNullOrWhiteSpace(cell))
+                    emails.Add(cell.Trim());
+            }
+
+            return emails;
+        }
+    }
+}
